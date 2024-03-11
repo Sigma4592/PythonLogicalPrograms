cd38bda401e83bf5b1be0a39ed2282e54b1456f6
Sigma4592 <140090255+Sigma4592@users.noreply.github.com>
2024-03-11 22:44:40 +0530

    Add files via upload

---
+++ 1.swapnum.py
@@ -0,0 +1,11 @@
+a = 25
+b = 30
+
+print("Before swaping - a = ", a)
+print("Before swaping - b = ", b)
+
+c = a + b
+b = c - b
+a = c - a
+print("After swaping - a = ", a)
+print("After swaping - a = ", b)
---
+++ 1a.swapnum_without_third.py
@@ -0,0 +1,10 @@
+num1 = input("Enter num1 value")
+num2 = input("Enter num2 value")
+
+print("Num1 value before swapping:",num1)
+print("Num2 value before swapping:",num2)
+
+num1,num2 = num2,num1
+
+print("Num1 value after swapping:",num1)
+print("Num2 value after swapping:",num2)
